bac7a1f8a798145d159963424ae25e26c77f8098
Gazelle90 <ghazaleh.ranji90@gmail.com>
2026-08-26 16:36:53 +0200

    time management initiations

--- src/time_management_app.py
+++ src/time_management_app.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, jsonify, abort
 from src.data.queries import (
-    list_persons, find_person_by_id, add_person, update_person,
-    delete_person,
+    list_customers, find_consultant_by_id ,list_consultants, find_consultant_by_id, add_time_entry, find_time_entries,
+    delete_time_entries,reporting_balance
 )
