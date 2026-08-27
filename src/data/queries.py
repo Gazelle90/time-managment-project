36c6519d14b61f1266c61b895d7232d7137d9f15
Gazelle90 <ghazaleh.ranji90@gmail.com>
2026-08-27 16:00:15 +0200

    finalized queries

--- src/data/queries.py
+++ src/data/queries.py
@@ -1,16 +1,17 @@
-import psycopg2
+
 from datetime import date, time
+from src.data.database import get_connection
 
 #functions related to customers or comanies 
 def list_customers() -> list[tuple]:
     con = None
     try:
-        con = psycopg2.connect(**load_db_config())
+        con = get_connection()
         cursor = con.cursor()
         cursor.execute( "SELECT * FROM company")
         rows = cursor.fetchall()
 
-    except (Exception, psycopg2.DatabaseError) as error:
+    except (Exception) as error:
         if con is not None:
             con.rollback()
         raise
@@ -23,11 +24,11 @@ def find_customer_by_id(company_id: int) -> tuple | None:
 
     con = None
     try:
-        con = psycopg2.connect(**load_db_config())
+        con = get_connection()
         cursor = con.cursor()
         cursor.execute("SELECT * FROM company WHERE id = %s" , (company_id,))
         rows = cursor.fetchone()
-    except (Exception, psycopg2.DatabaseError) as error:
+    except (Exception) as error:
         if con is not None:
             con.rollback()
         raise
@@ -41,12 +42,12 @@ def find_customer_by_id(company_id: int) -> tuple | None:
 def list_consultants() -> list[tuple]:
     con = None
     try:
-        con = psycopg2.connect(**load_db_config())
+        con = get_connection()
         cursor = con.cursor()
         cursor.execute( "SELECT * FROM person")
         rows = cursor.fetchall()
 
-    except (Exception, psycopg2.DatabaseError) as error:
+    except (Exception) as error:
         if con is not None:
             con.rollback()
         raise
@@ -59,11 +60,11 @@ def list_consultants() -> list[tuple]:
 def find_consultant_by_id(person_id: int) -> tuple | None:
     con = None
     try:
-        con = psycopg2.connect(**load_db_config())
+        con = get_connection()
         cursor = con.cursor()
         cursor.execute("SELECT * FROM person WHERE id = %s" , (person_id,))
         rows = cursor.fetchone()
-    except (Exception, psycopg2.DatabaseError) as error:
+    except (Exception) as error:
         if con is not None:
             con.rollback()
         raise
@@ -76,40 +77,53 @@ def find_consultant_by_id(person_id: int) -> tuple | None:
 # timeentries 
 
 def add_time_entry(person_id: int, company_id: int ,date : date ,start_time: time,
-                   end_time: time, lunch_break: int) -> tuple:
+                   end_time: time, lunch_break: time) -> tuple:
     con = None
     try:
-        con = psycopg2.connect(**load_db_config())
+        con = get_connection()
         cursor = con.cursor()
         cursor.execute("INSERT INTO times_daily"
-        " ( person_id, company_id,date, start_time, end_time, lunch_break) " \
-        "VALUES (%s,%s,%s,%s,%s,%s)",
+        " ( person_id, company_id, date, start_time, end_time, lunch_break) " \
+        "VALUES (%s,%s,%s,%s,%s,%s)"\
+            "RETURNING id",
         (person_id, company_id,date, start_time, end_time, lunch_break))
-
         row = cursor.fetchone()
-    except (Exception, psycopg2.DatabaseError) as error:
+        con.commit()
+        return row[0]
+    except (Exception) as error:
         if con is not None:
             con.rollback()
         raise
     finally:
         if con is not None:
             con.close()
-    return row
+    
 
 def find_time_entries(person_id: int,company_id: int) -> tuple:
     con = None
     try:
-        con = psycopg2.connect(**load_db_config())
+        con = get_connection()
         cursor = con.cursor()
         cursor.execute(
-                """
-                SELECT * FROM times_daily
-                WHERE person_id = %s AND company_id = %s
-                """,
-                (person_id, company_id))
+        """
+        SELECT
+            id,
+            person_id,
+            company_id,
+            date,
+            start_time,
+            end_time,
+            lunch_break,
+            hours
+        FROM times_daily
+        WHERE person_id = %s AND company_id = %s
+        """,
+    (person_id, company_id)
+)
+
         rows = cursor.fetchall()
         return rows
-    except psycopg2.DatabaseError:
+    except (Exception) as error:
         if con is not None:
             con.rollback()
         raise
@@ -117,12 +131,13 @@ def find_time_entries(person_id: int,company_id: int) -> tuple:
         if con is not None:
             con.close()
 
+
      
 
-def delete_time_entries(person_id: int,company_id: int, work_date: date, start_time: time) -> int:
+def delete_time_entries(person_id: int,company_id: int, work_date: date) -> int:
     con = None
     try:
-        con = psycopg2.connect(**load_db_config())
+        con = get_connection()
         cursor = con.cursor()
         cursor.execute(
             """
@@ -130,14 +145,14 @@ def delete_time_entries(person_id: int,company_id: int, work_date: date, start_t
             WHERE person_id = %s
               AND company_id = %s
               AND date = %s
-              AND start_time = %s
+
             """,
-            (person_id, company_id, work_date, start_time)
+            (person_id, company_id, work_date)
         )
         rows = cursor.rowcount
         con.commit()
         return rows
-    except psycopg2.DatabaseError:
+    except (Exception) as error:
         if con is not None:
             con.rollback()
         raise
@@ -149,12 +164,12 @@ def delete_time_entries(person_id: int,company_id: int, work_date: date, start_t
 def reporting_balance(person_id: int, company_id: int ,date : date) -> tuple:
     con = None
     try:
-        con = psycopg2.connect(**load_db_config())
+        con = get_connection()
         cursor = con.cursor()
         #TODO : fill in the query
         cursor.execute("")
         rows = cursor.fetchall()
-    except (Exception, psycopg2.DatabaseError) as error:
+    except (Exception) as error:
         if con is not None:
             con.rollback()
         raise
